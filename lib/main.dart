7a0e53da1039647ca265cd9d52936f4553412937
ttoghere <59641232+ttoghere@users.noreply.github.com>
2023-09-20 17:32:47 +0300

    What i did ? && Next on:
    Did:
    1)Project UI Started
    2)Retrieved the code from old repository what includes:
    *Utils:
    -Color Constants
    -Info Dummy Datas
    -Responsive Settings
    *Widgets:
    **Shared:
    -Chat List
    -Contacts List
    -My Message Card
    -Sender Message Card
    **Web
    -Web Chat AppBar
    -Web Profile Bar
    -Web Search Bar
    *Screens
    -Mobile Chat Screen
    -Mobile Layout Screen
    -Web Layout Screen
    Next on:
    Re make and update the project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:tuncchat/screens/screens.dart';
+import 'package:tuncchat/utils/utils.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'mobile_chat_screen.dart';
+export 'mobile_layout_screen.dart';
+export 'web_layout_screen.dart';
